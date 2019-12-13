fa6548f2be46e26e10741b3d9af8e57163d1d8ee
Amit Jindal <AJindal@nationalindemnity.com>
2019-12-13 14:15:44 +0530

    sec

---
+++ XTabAPI/Controllers/TestController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace XTabAPI.Controllers
+{
+    public class TestController : ApiController
+    {
+        [HttpGet]
+        public IHttpActionResult GetDetails()
+        {
+            Debug.Write($"AuthenticationType: {User.Identity.AuthenticationType}");
+            Debug.Write($"IsAuthenticated: {User.Identity.IsAuthenticated}");
+            Debug.Write($"Name: {User.Identity.Name}");
+
+
+
+            return Ok("Authenticated: " + User.Identity.Name + "- This is Test Response");
+            // return Ok("Authenticated: yourdomain1\\yourUserName1 - This is Test Response");
+        }
+    }
+
+}
